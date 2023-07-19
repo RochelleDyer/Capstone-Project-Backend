fd48756a717e57823fe1e470f67603837a367339
Rochelle Dyer <rochelle.dyer@gmail.com>
2023-07-19 12:37:29 -0400

    Capstone Backend

--- app/site/routes.py
+++ app/site/routes.py
@@ -4,7 +4,7 @@ site = Blueprint('site', __name__, template_folder='site_templates')
 
 @site.route('/')
 def home():
-    return render_template('http://localhost:5173/')
+    return render_template('index.html')
 
 @site.route('/profile')
 def profile():
